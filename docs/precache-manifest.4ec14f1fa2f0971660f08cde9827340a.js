d6f2f488091f2b1e86085898d73fbcb787c766f0
Wilmer Fontt <wfonttbrito@gmail.com>
2020-07-23 15:27:03 -0500

    docs agragado

---
+++ docs/precache-manifest.4ec14f1fa2f0971660f08cde9827340a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3bd47a52810181108e2fd25ba3ca2e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad7641e1ae02195d4b6f",
+    "url": "/static/css/main.a43479a7.chunk.css"
+  },
+  {
+    "revision": "99e680c708e8e4e99ff6",
+    "url": "/static/js/2.086f22a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.086f22a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad7641e1ae02195d4b6f",
+    "url": "/static/js/main.22ab7ead.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
